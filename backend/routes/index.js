7085fc414b7b294fe3aea4c1c903b3629dd07907
DezmonDND <denedoseikin@mail.ru>
2024-01-10 20:51:50 +0700

    fix: настроить eslint

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,9 +5,9 @@ const auth = require('../middlewares/auth');
 const { createUser, login } = require('../controllers/users');
 const { loginValidation, createUserValidation } = require('../middlewares/validationJoi');
 
-routes.post('/signin', loginValidation, login);
-routes.post('/signup', createUserValidation, createUser);
 routes.use('/users', auth, usersRouter);
 routes.use('/cards', auth, cardsRouter);
+routes.post('/signin', loginValidation, login);
+routes.post('/signup', createUserValidation, createUser);
 
 module.exports = routes;
