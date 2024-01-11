44f1cbff6b1b9a3894f41d175cd435a277023fe5
DezmonDND <denedoseikin@mail.ru>
2024-01-11 12:28:28 +0700

    fix: исправить проверку токена при запросах

--- backend/errors/errorHandlers/ServerErrorHandler.js
+++ backend/errors/errorHandlers/ServerErrorHandler.js
@@ -1,4 +1,6 @@
-module.exports.ServerErrorHandler = (err, req, res) => {
+/* eslint-disable */
+
+module.exports.ServerErrorHandler = (err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res.status(statusCode).send({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
 };
