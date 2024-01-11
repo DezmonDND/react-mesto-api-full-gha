c47fee8c619811155f8ac0d0141596d616620cd5
DezmonDND <denedoseikin@mail.ru>
2024-01-11 19:58:29 +0700

    fix: настроить переменные окружения

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -6,8 +6,6 @@ const NotFoundError = require('../errors/NotFoundError');
 const BadRequestError = require('../errors/BadRequestError');
 const AlreadyExistsError = require('../errors/AlreadyExistsError');
 
-const { NODE_ENV, JWT_SECRET } = process.env;
-
 module.exports.createUser = (req, res, next) => {
   const {
     name, about, avatar, email, password,
@@ -132,7 +130,9 @@ module.exports.login = (req, res, next) => {
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
+      const { NODE_ENV, JWT_SECRET } = process.env;
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
+      console.log(JWT_SECRET);
       res.send({ token });
     })
     .catch((err) => next(err));
